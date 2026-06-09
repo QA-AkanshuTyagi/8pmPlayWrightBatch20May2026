03e9d59c3e5dc7204d0695d9f50d8a73365596dd
QA-AkanshuTyagi <tyagiakanshu2@gmail.com>
2026-06-09 20:48:06 +0530

    moving latest code

---
+++ 01KeyWords.js
@@ -0,0 +1,20 @@
+
+
+function add(){
+   let a=20;
+     a=10;
+    var b=20;
+
+console.log(a);
+}
+add();
+
+if(true){
+var name="akanshu";
+
+var isMarried=false;
+
+let country=null;
+
+}
+console.log(name);
