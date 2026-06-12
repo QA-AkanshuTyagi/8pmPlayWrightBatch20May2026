1613c0fc97f50113594301cf19930bac9d6422d7
Aishu1222 <aishumahadik.am@gmail.com>
2026-06-12 12:27:59 +0530

    Moving For loops, nestedif file

---
+++ Aishwarya/ArrayConcept.js
@@ -0,0 +1,16 @@
+let names = ["Aishu" , "Mayur" , "Varsha", "Anil"];
+console.log(names[2]);
+console.log(names.length);
+
+console.log(names[1]+","+names[3]);
+
+names.push("Aru");
+console.log(names);
+
+names.pop();
+console.log(names);
+names.unshift("Aru");
+console.log(names);
+
+names.shift();
+console.log(names);
