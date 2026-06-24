3a0f2cf1952a22c30c46e96117d0c2f1f0a17385
Aishu1222 <aishumahadik.am@gmail.com>
2026-06-24 20:08:52 +0530

    Update  File

---
+++ Akanshu/Object.js
@@ -0,0 +1,25 @@
+let names= {
+name1: "akanshu",
+name2: "rahul",
+name3: "sachin",
+name4: "rohit",
+name5: "virat"
+
+}
+
+names.name1="Aishwarya"
+
+names.name6=12
+console.log(names["name1"])
+console.log(names.name6)
+
+let bioData={
+
+names: ["akanshu", "rahul", "sachin", "rohit", "virat"],  
+ages: [22, 23, 24, 25, 26],
+pincodes: [110001, 110002, 110003, 110004, 110005]
+
+
+}
+
+console.log(bioData.names[3]);
