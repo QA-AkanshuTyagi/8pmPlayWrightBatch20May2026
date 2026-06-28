9a63c3e64e70523684fcaa5b28c4d4266be70843
Aishu1222 <aishumahadik.am@gmail.com>
2026-06-28 15:30:15 +0530

    Moving Function and concatination file

---
+++ Aishwarya/ConcatinationConcept.js
@@ -0,0 +1,10 @@
+let firstname = "Aishwarya";
+let lastname = "Mahadik";
+
+console.log(firstname+lastname);
+console.log(firstname,lastname);
+console.log(firstname.concat(lastname));
+console.log(firstname +" "+lastname);
+console.log(firstname+"-"+lastname);
+console.log(`${firstname} ${lastname}`);
+console.log(`${firstname} ${"*****"} ${lastname}`);
