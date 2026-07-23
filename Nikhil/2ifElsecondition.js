75317f42ffd8af54147218a65c35618e5306dd9c
QA-NikhilPal <np6529023@gmail.com>
2026-07-23 21:04:56 +0530

    late work

---
+++ Nikhil/2ifElsecondition.js
@@ -0,0 +1,8 @@
+let name="nikhil";
+
+if ((name=="nikhil")){
+    console.log("you are allowed to enter the park");
+}
+else{
+    console.log("you are not allowed to enter the park");
+}
---
+++ Nikhil/3ifElseifcondition.js
@@ -0,0 +1,15 @@
+let browers = 'yahoo';
+
+if((browers=='chrome')){
+console.log("launching chrome browser"); 
+}
+  else if(browers=='yahoo'){
+console.log("launching yahoo browser");
+} 
+  else if(browers=='edge'){
+console.log("launching edge browser");
+}
+{
+    console.log("please pass the correct brower name");
+
+}
