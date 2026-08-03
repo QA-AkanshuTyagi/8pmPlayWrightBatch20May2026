dbe0aa3d0d3bb7e9a7ce6d3c15cd5c9d2ecc6478
QA-NikhilPal <np6529023@gmail.com>
2026-08-03 19:27:46 +0530

    up to date

---
+++ Nikhil/5forLoop.js
@@ -0,0 +1,17 @@
+for(let i=2; i<=100; i++)
+{ 
+    let isprime= true;
+    for(let j=2; j<i;j++)
+    {
+        if(i%j==0)
+        {
+            isprime=false;
+            break;
+        }
+
+    }
+    if(isprime==true)
+    {
+        console.log(i);
+    }
+}
